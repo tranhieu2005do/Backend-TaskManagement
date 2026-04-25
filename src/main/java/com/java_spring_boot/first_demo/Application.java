24d779f3a094b64ccc0a7132828a34dd5c872118
Do Tran Hieu <dotranhieu925@gmail.com>
2026-04-25 17:39:46 +0700

    Test cicd

--- src/main/java/com/java_spring_boot/first_demo/Application.java
+++ src/main/java/com/java_spring_boot/first_demo/Application.java
@@ -18,6 +18,7 @@ public class Application {
 	public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
+        System.out.println("Checking line");
 	}
 
 }
