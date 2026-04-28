aa0014fcc2ccf920a4dedcb9b734a9210da83671
Do Tran Hieu <dotranhieu925@gmail.com>
2026-04-28 09:18:37 +0700

    Update fix

--- src/main/java/com/java_spring_boot/first_demo/Application.java
+++ src/main/java/com/java_spring_boot/first_demo/Application.java
@@ -18,7 +18,6 @@ public class Application {
 	public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
-        System.out.println("Checking line");
 	}
 
 }
